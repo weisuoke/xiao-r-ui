62eec96121bb2cef54fb192f97609f5d6419b7b8
梅林的法杖 <530264127@qq.com>
2021-08-06 19:06:49 +0800

    feat: integrate storybook

---
+++ packages/button/button.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './index';
+
+export default {
+  title: 'XiaoUI/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+};
